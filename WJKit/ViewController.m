609f3ec1d3b9dc73ae4b4cd61ba84e2044183e06
virgiltangxhj <virgiltangxhj@gmail.com>
2017-03-06 16:15:02 +0800

    Initial Commit

---
+++ WJKit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WJKit
+//
+//  Created by XHJ on 2017/3/6.
+//  Copyright © 2017年 XHJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
