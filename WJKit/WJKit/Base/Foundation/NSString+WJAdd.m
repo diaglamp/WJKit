589a24caeeb0dbd34fc3326d1b53ded83b85e5f0
virgiltangxhj <virgiltangxhj@gmail.com>
2017-03-14 20:42:09 +0800

    NSString+add

---
+++ WJKit/WJKit/Base/Foundation/NSString+WJAdd.m
@@ -0,0 +1,92 @@
+//
+//  NSString+WJAdd.m
+//  WJKit
+//
+//  Created by XHJ on 2017/3/14.
+//  Copyright © 2017年 XHJ. All rights reserved.
+//
+
+#import "NSString+WJAdd.h"
+#import "WJKitMacro.h"
+
+WJSYNTH_DUMMY_CLASS(NSString_WJAdd)
+
+@implementation NSString (WJAdd)
+
+#pragma mark - Drawing
+///=============================================================================
+/// @name Drawing
+///=============================================================================
+- (CGSize)sizeForFont:(UIFont *)font size:(CGSize)size mode:(NSLineBreakMode)lineBreakMode {
+    NSMutableDictionary *attr = [NSMutableDictionary new];
+    attr[NSFontAttributeName] = font;
+    if (lineBreakMode != NSLineBreakByWordWrapping) {
+        NSMutableParagraphStyle *paragraphStyle = [NSMutableParagraphStyle new];
+        paragraphStyle.lineBreakMode = lineBreakMode;
+        attr[NSParagraphStyleAttributeName] = paragraphStyle;
+    }
+    CGRect rect = [self boundingRectWithSize:size
+                                     options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading
+                                  attributes:attr context:nil];
+    return rect.size;
+}
+- (CGFloat)widthForFont:(UIFont *)font {
+    CGSize size = [self sizeForFont:font size:CGSizeMake(HUGE, HUGE) mode:NSLineBreakByWordWrapping];
+    return size.width;
+}
+
+- (CGFloat)heightForFont:(UIFont *)font width:(CGFloat)width {
+    CGSize size = [self sizeForFont:font size:CGSizeMake(width, HUGE) mode:NSLineBreakByWordWrapping];
+    return size.height;
+}
+
++ (CGFloat)heightForFontSize:(CGFloat)fontSize {
+    NSString *text = @"text";
+    return [text heightForFont:[UIFont systemFontOfSize:fontSize] width:HUGE];
+}
+
+#pragma mark - Regular Expression
+///=============================================================================
+/// @name Regular Expression
+///=============================================================================
+- (BOOL)matchesRegex:(NSString *)regex options:(NSRegularExpressionOptions)options {
+    NSRegularExpression *pattern = [NSRegularExpression regularExpressionWithPattern:regex options:options error:NULL];
+    if (!pattern) return NO;
+    return ([pattern numberOfMatchesInString:self options:0 range:NSMakeRange(0, self.length)] > 0);
+}
+
+- (void)enumerateRegexMatches:(NSString *)regex
+                      options:(NSRegularExpressionOptions)options
+                   usingBlock:(void (^)(NSString *match, NSRange matchRange, BOOL *stop))block {
+    if (regex.length == 0 || !block) return;
+    NSRegularExpression *pattern = [NSRegularExpression regularExpressionWithPattern:regex options:options error:nil];
+    if (!regex) return;
+    [pattern enumerateMatchesInString:self options:kNilOptions range:NSMakeRange(0, self.length) usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
+        block([self substringWithRange:result.range], result.range, stop);
+    }];
+}
+
+- (NSString *)stringByReplacingRegex:(NSString *)regex
+                             options:(NSRegularExpressionOptions)options
+                          withString:(NSString *)replacement; {
+    NSRegularExpression *pattern = [NSRegularExpression regularExpressionWithPattern:regex options:options error:nil];
+    if (!pattern) return self;
+    return [pattern stringByReplacingMatchesInString:self options:0 range:NSMakeRange(0, [self length]) withTemplate:replacement];
+}
+
+#pragma mark - Utilities
+///=============================================================================
+/// @name Utilities
+///=============================================================================
++ (NSString *)stringWithUUID {
+    CFUUIDRef uuid = CFUUIDCreate(NULL);
+    CFStringRef string = CFUUIDCreateString(NULL, uuid);
+    CFRelease(uuid);
+    return (__bridge_transfer NSString *)string;
+}
+
+- (NSString *)stringByTrim {
+    NSCharacterSet *set = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+    return [self stringByTrimmingCharactersInSet:set];
+}
+@end
