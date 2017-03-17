b21763cd135c515c4e2cb16883eddad0cfa06f1d
virgiltangxhj <virgiltangxhj@gmail.com>
2017-03-17 16:38:56 +0800

    change directory

---
+++ WJKit/WJKit/Base/UIKit/UIImage+WJAdd.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+WJAdd.m
+//  WJKit
+//
+//  Created by XHJ on 2017/3/17.
+//  Copyright © 2017年 XHJ. All rights reserved.
+//
+
+#import "UIImage+WJAdd.h"
+
+@implementation UIImage (WJAdd)
+
+@end
