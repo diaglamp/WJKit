87405de8335717c4523c11e3770e6d050143bd6b
virgiltangxhj <virgiltangxhj@gmail.com>
2017-04-01 18:25:44 +0800

    Image test

--- WJKit/Base/UIKit/UIImage+WJAdd.m
+++ WJKit/Base/UIKit/UIImage+WJAdd.m
@@ -324,18 +324,18 @@ static NSTimeInterval _wj_CGImageSourceGetGIFFrameDelayAtIndex(CGImageSourceRef 
         CGContextRestoreGState(context);
     }
     
-    if (borderColor && borderWidth < minSize / 2 && borderWidth > 0) {
-        CGFloat strokeInset = (floor(borderWidth * self.scale) + 0.5) / self.scale;
-        CGRect strokeRect = CGRectInset(rect, strokeInset, strokeInset);
-        CGFloat strokeRadius = radius > self.scale / 2 ? radius - self.scale / 2 : 0;
-        UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:strokeRect byRoundingCorners:corners cornerRadii:CGSizeMake(strokeRadius, borderWidth)];
-        [path closePath];
-        
-        path.lineWidth = borderWidth;
-        path.lineJoinStyle = borderLineJoin;
-        [borderColor setStroke];
-        [path stroke];
-    }
+//    if (borderColor && borderWidth < minSize / 2 && borderWidth > 0) {
+//        CGFloat strokeInset = (floor(borderWidth * self.scale) + 0.5) / self.scale;
+//        CGRect strokeRect = CGRectInset(rect, strokeInset, strokeInset);
+//        CGFloat strokeRadius = radius > self.scale / 2 ? radius - self.scale / 2 : 0;
+//        UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:strokeRect byRoundingCorners:corners cornerRadii:CGSizeMake(strokeRadius, borderWidth)];
+//        [path closePath];
+//        
+//        path.lineWidth = borderWidth;
+//        path.lineJoinStyle = borderLineJoin;
+//        [borderColor setStroke];
+//        [path stroke];
+//    }
     
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
--- WJKitDemo/ViewController.m
+++ WJKitDemo/ViewController.m
@@ -13,12 +13,38 @@
 
 @interface ViewController ()
 @property (nonatomic, strong) UIImageView *imageView;
+@property (nonatomic, strong) UIImageView *originView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self roundCornerTest];
+    
+}
+
+- (void)roundCornerTest {
+    UIImage *image = [[UIImage imageNamed:@"two_cat"] imageByRoundCornerRadius:0
+                                                                        corners:UIRectCornerAllCorners
+                                                                    borderWidth:10
+                                                                    borderColor:[UIColor blueColor]
+                                                                 borderLineJoin:kCGLineJoinMiter];
+    _imageView = [[UIImageView alloc]initWithImage:image];
+    _imageView.frame = CGRectMake(100, 100, 0, 0);
+    [_imageView sizeToFit];
+    [self.view addSubview:_imageView];
+    
+    UIImage *imageO = [UIImage imageNamed:@"two_cat"];
+    _originView = [[UIImageView alloc]initWithImage:imageO];
+    _originView.frame = CGRectMake(100, 300, 0, 0);
+    [_originView sizeToFit];
+    [self.view addSubview:_originView];
+    
+}
+
+- (void)smallGifTest {
     NSString *resPath = [[NSBundle mainBundle] resourcePath];
     NSString *bundlePath = [resPath stringByAppendingPathComponent:@"EmoticonQQ.bundle"];
     NSBundle *bundle = [NSBundle bundleWithPath:bundlePath];
@@ -29,13 +55,6 @@
     _imageView.frame = CGRectMake(100, 100, 0, 0);
     [_imageView sizeToFit];
     [self.view addSubview:_imageView];
-    
-    CFArrayRef mySourceTypes = CGImageSourceCopyTypeIdentifiers();
-    CFShow(mySourceTypes);
-    
-    NSDate *date = WJCompileTime();
-    NSLog(@"date = %@",date);
 }
-
 
 @end
