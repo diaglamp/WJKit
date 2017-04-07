fd52252728472bc3fdf7dc48210cc4548a9fa63a
virgiltangxhj <virgiltangxhj@gmail.com>
2017-04-07 10:21:45 +0800

    UIScrollview+Add

---
+++ WJKit/Base/UIKit/UIScrollView+WJAdd.m
@@ -0,0 +1,56 @@
+//
+//  UIScrollView+WJAdd.m
+//  WJKit
+//
+//  Created by XHJ on 2017/4/6.
+//  Copyright © 2017年 XHJ. All rights reserved.
+//
+
+#import "UIScrollView+WJAdd.h"
+#import "WJKitMacro.h"
+
+WJSYNTH_DUMMY_CLASS(UIScrollView_WJAdd)
+
+@implementation UIScrollView (WJAdd)
+
+- (void)scrollToTop {
+    [self scrollToTopAnimated:YES];
+}
+
+- (void)scrollToBottom {
+    [self scrollToBottomAnimated:YES];
+}
+
+- (void)scrollToLeft {
+    [self scrollToLeftAnimated:YES];
+}
+
+- (void)scrollToRight {
+    [self scrollToRightAnimated:YES];
+}
+
+- (void)scrollToTopAnimated:(BOOL)animated {
+    CGPoint off = self.contentOffset;
+    off.y = 0 - self.contentInset.top;
+    [self setContentOffset:off animated:animated];
+}
+
+- (void)scrollToBottomAnimated:(BOOL)animated {
+    CGPoint off = self.contentOffset;
+    off.y = self.contentSize.height - self.bounds.size.height + self.contentInset.bottom;
+    [self setContentOffset:off animated:animated];
+}
+
+- (void)scrollToLeftAnimated:(BOOL)animated {
+    CGPoint off = self.contentOffset;
+    off.x = 0 - self.contentInset.left;
+    [self setContentOffset:off animated:animated];
+}
+
+- (void)scrollToRightAnimated:(BOOL)animated {
+    CGPoint off = self.contentOffset;
+    off.x = self.contentSize.width - self.bounds.size.width + self.contentInset.right;
+    [self setContentOffset:off animated:animated];
+}
+
+@end
