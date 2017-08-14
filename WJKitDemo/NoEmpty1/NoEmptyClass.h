652ceef393e1b970c0a4b99075466f10dc780b0f
dengweijie <dengweijie@lizhi.fm>
2017-08-14 15:46:40 +0800

    added:test empty folder

---
+++ WJKitDemo/NoEmpty1/NoEmptyClass.h
@@ -0,0 +1,13 @@
+//
+//  NoEmptyClass.h
+//  WJKit
+//
+//  Created by dengweijie on 2017/8/14.
+//  Copyright © 2017年 XHJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NoEmptyClass : NSObject
+
+@end
---
+++ WJKitDemo/NoEmpty1/NoEmptyClass.m
@@ -0,0 +1,13 @@
+//
+//  NoEmptyClass.m
+//  WJKit
+//
+//  Created by dengweijie on 2017/8/14.
+//  Copyright © 2017年 XHJ. All rights reserved.
+//
+
+#import "NoEmptyClass.h"
+
+@implementation NoEmptyClass
+
+@end
